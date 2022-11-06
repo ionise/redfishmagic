ce34c70d692311409feef509411cb7188f280705
David Alderman <david.alderman@vuw.ac.nz>
2022-11-06 21:12:47 +1300

    Cleaned up some error handling

--- public/Get-RedfishAPIRoot.ps1
+++ public/Get-RedfishAPIRoot.ps1
@@ -3,11 +3,7 @@ function Get-RedfishAPIRoot {
     #API Ping function to confirm credentials are correct and add the version to the session
     param (
         [Parameter(Mandatory = $True)]
-        [string]$Target <#,
-        [Parameter(Mandatory = $True)]
-        [pscredential]$Credential
-        #>
-        
+        [string]$Target         
     )
     begin{
         Write-Verbose -Message "[BEGIN  ] $($myInvocation.MyCommand)"
@@ -16,18 +12,25 @@ function Get-RedfishAPIRoot {
     process{
         Write-Verbose -Message "[PROCESS] $($myInvocation.MyCommand)"
         try {
-            #$Result = Invoke-RedfishMethod -uri $Uri -Method Get
-            $Result = Invoke-RedfishMethod -uri $Uri -Method Get #-Credential $Credential
-            $($Result.Content | ConvertFrom-Json)
+            Write-Verbose -Message "Attempting to connect to APIRoot at $Uri"
+            $Result = Invoke-RedfishMethod -uri $Uri -Method Get
+            Switch ($Result.StatusCode){
+                404 {
+                    Write-Error -Exception "Not Found" -Message "API Endpoint was not found at $Target" -Category ObjectNotFound -RecommendedAction "Check that this endpoint is Redfish capable"
+                    throw
+                }
+                200 {
+                    Write-Verbose -Message "Succesfully connected to API Root"
+                    $($Result.Content | ConvertFrom-Json)
+                }
+            }
         }
         catch {
-            Write-Error "Failed to get the root API for some reason. Use -Verbose to see debug"
+            Write-Error "Failed to get the root API"
+            $_
         }
-
-
     }
     end{
         Write-Verbose -Message "[END    ] $($myInvocation.MyCommand)"
-
     }  
 }
