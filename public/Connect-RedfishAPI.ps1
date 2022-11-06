cc11e8dd5c08e319bdc7fbbf1c2babfa1de086cf
David Alderman <david.alderman@vuw.ac.nz>
2022-11-06 21:17:07 +1300

    Begin to work with many targets

--- public/Connect-RedfishAPI.ps1
+++ public/Connect-RedfishAPI.ps1
@@ -6,89 +6,85 @@ function Connect-RedfishAPI {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $True)]
-        [string]$IpAddress,
+        [string[]]$Target,
         [Parameter(Mandatory = $True)]
         [pscredential]$Credential
     )
     begin{
         Write-Verbose -Message "[BEGIN  ] $($myInvocation.MyCommand)"
-        $Creds = New-Object System.Management.Automation.PSCredential($Credential.UserName, $Credential.Password)
-        $Global:Endpoint = $IpAddress
     }
     process{
         Write-Verbose -Message "[PROCESS] $($myInvocation.MyCommand)"
-        try {
-            Write-Verbose -Message "Testing connection to RedfishAPIRoot"  
-            $Global:RedfishAPIRoot = Get-RedfishAPIRoot -Target $Global:Endpoint # -Credential $Credential
-            Write-Verbose -Message "Raw RedfishVersion: $($Global:RedfishAPIRoot.RedfishVersion)"
-            $RedfishVersion = $($Global:RedfishAPIRoot.RedfishVersion).replace('.', '')
-            Write-Verbose -Message "Parsed RedfishVersion: $($global:RedfishVersion)"
-            $uri = if($RedfishVersion -ge 160) {
-                    Write-Verbose -Message "$($global:RedfishVersion ) is bigger than 160"
-                    "https://$Global:Endpoint/SessionService/Sessions"
-                
-                }
-                elseif ($RedfishVersion -lt 160) {
-                    Write-Verbose -Message "$($global:RedfishVersion ) is smaller than 160"
-                    "https://$Global:Endpoint/redfish/v1/Sessions"
-                }
-                else {        
-                    Write-Error "`n- ERROR, Unable to determine Sessions URI from parsed Redfish version"
-                    throw
-                }
-
+        ForEach($Endpoint in $Target){
             try {
-                Write-Verbose -Message "Attempting Pre-Authentication to $Uri"
-                $Result = Invoke-RedfishMethod -uri $Uri -Method Get -Credential $Creds
-                Write-Verbose -Message "Result:`n$Result"
-                Switch($Result.StatusCode){
-                    200 {
-                        Write-Verbose -Message "StatusCode 200 - OK"
+                Write-Verbose -Message "Testing connection to RedfishAPIRoot"  
+                $RedfishAPIRoot = Get-RedfishAPIRoot -Target $Endpoint # -Credential $Credential
+                Write-Verbose -Message "Raw RedfishVersion: $($RedfishAPIRoot.RedfishVersion)"
+                $RedfishVersion = $($RedfishAPIRoot.RedfishVersion).replace('.', '')
+                Write-Verbose -Message "Parsed RedfishVersion: $($RedfishVersion)"
+                $uri = if($RedfishVersion -ge 160) {
+                        Write-Verbose -Message "$($RedfishVersion ) is bigger than 160"
+                        "https://$Endpoint/SessionService/Sessions"
+                    
                     }
-                    Default {
-                        Write-Verbose -Message "Unhandled status code during pre-authentication"
+                    elseif ($RedfishVersion -lt 160) {
+                        Write-Verbose -Message "$($RedfishVersion ) is smaller than 160"
+                        "https://$Endpoint/redfish/v1/Sessions"
+                    }
+                    else {        
+                        Write-Error "`n- ERROR, Unable to determine Sessions URI from parsed Redfish version"
+                        throw
+                    }
+                <# Might not need really need to do this bit 
+                try {
+                    Write-Verbose -Message "Attempting Pre-Authentication to $Uri"
+                    $Result = Invoke-RedfishMethod -uri $Uri -Method Get -Credential $Creds
+                    Write-Verbose -Message "Result:`n$Result"
+                    Switch($Result.StatusCode){
+                        200 {
+                            Write-Verbose -Message "StatusCode 200 - OK"
+                        }
+                        Default {
+                            Write-Verbose -Message "Unhandled status code during pre-authentication"
+                        }
                     }
                 }
+                catch {
+                        Write-Error "Pre-Authentication failed"
+                }
+                #>
+                Write-Verbose -Message "Sessions URI: $Uri"
+                $Body = @{
+                        'UserName' = $credential.GetNetworkCredential().UserName 
+                        'Password' = $credential.GetNetworkCredential().Password
+                }
+                try {
+                    $Result = Invoke-RedfishMethod -uri $Uri -Body $Body -Method Post -ContentType 'application/json'
+                    Write-Verbose -Message "Result:`n$Result"
+                    Switch($Result.StatusCode){
+                        201 {
+                            Write-Verbose -Message "StatusCode 201 - Resource Created"
+                            Write-Verbose -Message "Storing the session"
+                            $RedfishSession = $Result.Headers
+                        }
+                        Default{
+                            Write-Verbose -Message "Unhandled status code obtaining x-auth-token"
+                        }
+                    }
+                }
+                catch {
+                    Write-Verbose -Message "Could not obtain x-auth-token"
+                }
+                Write-Verbose "Result Headers:`n $($Result.Headers | ConvertTo-Json)"
             }
             catch {
-                    Write-Error "Pre-Authentication failed"
+                Write-Verbose "Unable to connect to API"
+                $_
             }
-            Write-Verbose -Message "Sessions URI: $Uri"
-            $Body = @{
-                    'UserName' = $credential.GetNetworkCredential().UserName 
-                    'Password' = $credential.GetNetworkCredential().Password
-            }
-            try {
-                $Result = Invoke-RedfishMethod -uri $Uri -Body $Body -Method Post -ContentType 'application/json'
-                Write-Verbose -Message "Result:`n$Result"
-                Switch($Result.StatusCode){
-                    201 {
-                        Write-Verbose -Message "StatusCode 201 - Resource Created"
-                        Write-Verbose -Message "Storing the session"
-                        $Global:RedfishSession = $Result.Headers
-                    }
-                    Default{
-                        Write-Verbose -Message "Unhandled status code obtaining x-auth-token"
-                    }
-                }
-
-
-            }
-            catch {
-                Write-Error -Message "Could not obtain x-auth-token"
-            }
-            Write-Verbose "Result Headers:`n $($Result.Headers| ConvertTo-Json)"
-
         }
-        catch {
-            $_
-            Write-Error "Unable to connect to API, use -Verbose to debug"
-        }
-
     }
     end{
         Write-Verbose -Message "[END    ] $($myInvocation.MyCommand)"
-
     }
 }
 
